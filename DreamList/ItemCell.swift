126a03ed5de4c2d818edccfb2cac37011bfdb62b
Lane Faison <lane.faison@icloud.com>
2017-05-29 22:30:39 -0600

    TableView linked to VC

---
+++ DreamList/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamList
+//
+//  Created by Lane Faison on 5/29/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+ 
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
